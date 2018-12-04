5024bfedd5925c33a9c69d8e9f7b2fbba8ed5d33
fdfzcq <fdfzcq@gmail.com>
2018-12-04 01:19:43 +0100

    Day 3: fix

--- src/slice_it.erl
+++ src/slice_it.erl
@@ -5,11 +5,11 @@
 -record(rectangle, {l_vertex, t_vertex, r_vertex, b_vertex}).
 
 process(Session) ->
-	Body = <<"#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 4,4: 4x4">>,
-	%{ok, Body} = advent_of_code_client:get(3, Session),
+	%Body = <<"#1 @ 1,3: 4x4\n#3 @ 1,1: 4x4">>,
+	{ok, Body} = advent_of_code_client:get(3, Session),
 	BinList = string:split(string:trim(Body), "\n", all),
 	RectList = lists:map(fun(Bin) -> to_rectangle(Bin) end, BinList),
-	SortFun = fun(R1, R2) -> R1#rectangle.t_vertex < R2#rectangle.t_vertex end,
+	SortFun = fun(R1, R2) -> R1#rectangle.t_vertex =< R2#rectangle.t_vertex end,
 	sweep(lists:sort(SortFun, RectList), 0, 0).
 
 to_rectangle(Bin) ->
@@ -25,21 +25,20 @@ to_rectangle(Bin) ->
 	#rectangle{l_vertex = LVertex, t_vertex = TVertex,
 		r_vertex = LVertex + HEdge - 1, b_vertex = TVertex + VEdge - 1}.
 
-sweep(_, 1010, Size) -> Size;
+sweep(_, 1000, Size) -> Size;
 sweep(List, Line, Size) ->
 	case lists:filter(fun(R) -> R#rectangle.l_vertex =< Line andalso R#rectangle.r_vertex >= Line end, List) of
 		[] -> sweep(List, Line + 1, Size);
-		[H|T] ->
-			V = find_overlap(T, H#rectangle.t_vertex, H#rectangle.b_vertex, 0),
+		L ->
+			V = find_overlap(L, 0, 0),
+			%find_overlap(T, H#rectangle.t_vertex, H#rectangle.b_vertex, 0),
 			sweep(List, Line + 1, Size + V)
 	end.
 
-find_overlap([], _, _, Size) -> Size;
-find_overlap([H|T], Top, Bottom, Size) when Top > Bottom ->
-	find_overlap(T, H#rectangle.t_vertex, H#rectangle.b_vertex, Size);
-find_overlap([#rectangle{t_vertex=TV, b_vertex=BV}|T], Top, Bottom, Size) ->
-	V = min(Bottom, BV) - max(Top, TV) + 1,
-	case V > 0 of
-		true -> find_overlap(T, min(Bottom, BV) + 1, max(Bottom, BV), Size + V);
-		false -> find_overlap(T, TV, BV, Size)
+find_overlap(_, 1000, Size) -> Size;
+find_overlap(List, Row, Size) ->
+	Filtered = lists:filter(fun(R) -> R#rectangle.t_vertex =< Row andalso R#rectangle.b_vertex >= Row end, List),
+	case lists:flatlength(Filtered) > 1 of
+		true -> find_overlap(List, Row + 1, Size + 1);
+		false -> find_overlap(List, Row + 1, Size)
 	end.
