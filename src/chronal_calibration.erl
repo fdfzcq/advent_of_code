b91b7a6ebf4405a40c8907fb19aa71a66e1357ab
fdfzcq <fdfzcq@gmail.com>
2018-12-08 22:53:21 +0100

    Day 1 part 2

--- src/chronal_calibration.erl
+++ src/chronal_calibration.erl
@@ -1,17 +1,40 @@
 -module(chronal_calibration).
 -behaviour(gen_server).
 %% aoc day 1 - frequency
--export([count/1, start_link/0, count_all/1]).
+-export([count/1, start_link/0, count_all/1, duplicate_freq/1]).
 -export([init/1, handle_call/3, handle_cast/2]).
+
+duplicate_freq(Session) ->
+	{ok, Body} = advent_of_code_client:get(1, Session),
+	BinList = string:split(string:trim(Body), "\n", all),
+	List = lists:map(fun(Bin) -> binary_to_list(Bin) end, BinList),
+	find_freq(List, [0], List).
+
+find_freq([], Freqs, List) -> find_freq(List, Freqs, List);
+find_freq([H|T], Freqs, List) ->
+	Res = count(H, hd(Freqs)),
+	case lists:member(Res, Freqs) of
+		false -> find_freq(T, [Res|Freqs], List);
+		true -> Res
+	end.
+
+% noserver mode	
+count([$+|T], Res) -> Res + to_integer(T);
+count([$-|T], Res) -> Res - to_integer(T).
+
+%% part 1
 
 start_link() ->
 	gen_server:start_link({local, chronal_calibration}, chronal_calibration, [], []).
 
-init(_) -> {ok, 0}.
+init(_) -> {ok, [0]}.
 
 count_all(Session) ->
 	{ok, Body} = advent_of_code_client:get(1, Session),
 	BinList = string:split(Body, "\n", all),
+	process(BinList).
+
+process(BinList) ->
 	Fun = fun(Bin) ->
 		Input = binary_to_list(Bin),
 		count(Input) end,
@@ -21,15 +44,18 @@ count([$+|T]) -> gen_server:cast(chronal_calibration, {plus, to_integer(T)});
 count([$-|T]) -> gen_server:cast(chronal_calibration, {minus, to_integer(T)});
 count(_) -> ignored.
 
-handle_cast({plus, N}, Frequency) ->
-	io:format("Current frequency ~p, change of +~p; resulting Frequency ~p~n",
-		[Frequency, N, N + Frequency]),
-	{noreply, N + Frequency};
-handle_cast({minus, N}, Frequency) ->
-	io:format("Current frequency ~p, change of -~p; resulting Frequency ~p~n",
-		[Frequency, N, Frequency - N]),
-	{noreply, Frequency - N}.
+handle_cast({plus, N}, Frequencies) ->
+	Frequency = hd(Frequencies),
+	%io:format("Current frequency ~p, change of +~p; resulting Frequency ~p~n",
+	%	[Frequency, N, N + Frequency]),
+	{noreply, [N + Frequency|Frequencies]};
+handle_cast({minus, N}, Frequencies) ->
+	Frequency = hd(Frequencies),
+	%io:format("Current frequency ~p, change of -~p; resulting Frequency ~p~n",
+	%	[Frequency, N, Frequency - N]),
+	{noreply, [Frequency - N|Frequencies]}.
 
+handle_call(get_state, _, Frequencies) -> {reply, Frequencies, Frequencies};
 handle_call(_, _, Frequency) -> Frequency.
 
 to_integer(List) -> to_integer(lists:reverse(List), 0, 0).
