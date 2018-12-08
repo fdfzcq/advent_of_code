87aee63026280f839e76767480e282cb4731e748
fdfzcq <fdfzcq@gmail.com>
2018-12-09 00:26:32 +0100

    Remove comments

--- src/sum_of_parts.erl
+++ src/sum_of_parts.erl
@@ -4,13 +4,6 @@
 
 process2(Session) ->
 	{ok, Body} = advent_of_code_client:get(7, Session),
-% 	Body = <<"Step C must be finished before step A can begin.
-% Step C must be finished before step F can begin.
-% Step A must be finished before step B can begin.
-% Step A must be finished before step D can begin.
-% Step B must be finished before step E can begin.
-% Step D must be finished before step E can begin.
-% Step F must be finished before step E can begin.">>,
 	BinList = string:split(string:trim(Body), "\n", all),
 	List = lists:map(fun(X) -> parse(X) end, BinList),
 	Map = to_map(List, #{}),
