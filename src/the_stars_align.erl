d4844d179b75cbcc99e995506508dcb8a0420a92
fdfzcq <fdfzcq@gmail.com>
2018-12-10 22:50:53 +0100

    Day 10

---
+++ src/the_stars_align.erl
@@ -0,0 +1,53 @@
+-module(the_stars_align).
+%% Day 10
+-export([process/1, plot/1]).
+
+process(Session) ->
+	{ok, Body} = advent_of_code_client:get(10, Session),
+	BinList = string:split(string:trim(Body), "\n"),
+	List = lists:map(fun(Bin) -> parse(Bin) end, BinList),
+	start(List, 0, 0).
+
+start(_, Second, Second) -> ok;
+start(List, Second, LastSecond) ->
+	plot(calculate_velocity(List, Second, [])),
+	start(List, Second + 1, LastSecond).
+
+calculate_velocity([], _, List) -> List;
+calculate_velocity([{XP, YP, XV, YV}|T], Second, List) ->
+	calculate_velocity(T, Second, [{XP + XV * Second, YP + YV * Second}|List]).
+
+parse(Bin) ->
+	Str = binary_to_list(Bin),
+	{match, L} =
+		re:run(Str, ".*<(?<xp>[\\s-0-9]+),(?<yp>[\\s-\\d]+)>.*<(?<xv>[\\s-\\d]+),(?<yv>[\\s-\\d]+)>", [{capture, [xp, yp, xv, yv], list}]),
+	[XP, YP, XV, YV] = lists:map(fun(S) -> list_to_integer(string:trim(S)) end, L),
+	{XP, YP, XV, YV}.
+
+plot(List) ->
+	ListX = lists:map(fun({X, _}) -> X end, List),
+	ListY = lists:map(fun({_, Y}) -> Y end, List),
+	X_MIN = lists:min(ListX),
+	X_MAX = lists:max(ListX),
+	Y_MIN = lists:min(ListY),
+	Y_MAX = lists:max(ListY),
+	SortedList = lists:sort(fun({X1, Y1}, {X2, Y2}) ->
+		case Y1 == Y2 of
+			true -> X1 < X2;
+			false -> Y1 < Y2
+		end
+	end, List),
+	plot(SortedList, X_MIN, Y_MIN, {X_MIN, X_MAX}, {Y_MIN, Y_MAX}, "").
+
+plot(_, _, Y, _, {_, Y_MAX}, _) when Y > Y_MAX -> ok;
+plot(List, X, Y, {X_MIN, X_MAX}, {Y_MIN, Y_MAX}, Res) when X > X_MAX ->
+	%io:format("~p~n", [Res]),
+	case length(lists:filter(fun(P) -> P == $# end, Res)) > 1 of
+		true -> io:format("~p~n", [lists:reverse(Res)]);
+		false -> ok
+	end,
+	plot(List, X_MIN, Y + 1, {X_MIN, X_MAX}, {Y_MIN, Y_MAX}, "");
+plot([{X, Y}|T], X, Y, {X_MIN, X_MAX}, {Y_MIN, Y_MAX}, Res) ->
+	plot(T, X + 1, Y, {X_MIN, X_MAX}, {Y_MIN, Y_MAX}, [$#|Res]);
+plot(List, X, Y, {X_MIN, X_MAX}, {Y_MIN, Y_MAX}, Res) ->
+	plot(List, X + 1, Y, {X_MIN, X_MAX}, {Y_MIN, Y_MAX}, [$.|Res]).
