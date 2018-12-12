02eeb328ed4a43a26330c5333f8dd1d0ac17c7cb
fdfzcq <fdfzcq@gmail.com>
2018-12-12 01:12:11 +0100

    Day 11

--- src/chronal_charge.erl
+++ src/chronal_charge.erl
@@ -3,39 +3,60 @@
 -export([process1/1, calculate_size/5, process2/1]).
 
 process2(GSN) ->
-	process2(GSN, 299, {nil, -45, 0}, #{}).
+	List = process2(GSN, {1, 1}, [[]]),
+	find_max(List, {1,1}, 0, 0, {-100, nil, 0}).
 
-process2(_, 0, {P, _, Size}, _) -> {P, Size};
-process2(GSN, L, {P, Power, Size}, Map) ->
-	case (L + 1) * (L + 1) * 4 < Power of
-		true -> {P, Size};
-		false -> {NewMap, {NewP, NewPower}} = process1(GSN, {1, 1}, Map, {P, Power}, L),
-				 case NewPower > Power of
-				 	true -> process2(GSN, L - 1, {NewP, NewPower, (L + 1) * (L + 1)}, NewMap);
-				 	false -> process2(GSN, L - 1, {P, Power, Size}, NewMap)
-				 end
+process2(_, {301, 301}, List) -> lists:reverse(List);
+process2(GSN, {301, Y}, [H|T]) -> process2(GSN, {1, Y + 1}, [[], lists:reverse(H)|T]);
+process2(GSN, {X, Y}, [Row|T]) ->
+	process2(GSN, {X + 1, Y}, [[calculate_power({X, Y}, GSN)|Row]|T]).
+
+
+find_max(_, {301, 301}, _, _, {MaxPower, P, Size}) ->
+	{MaxPower, P, Size};
+find_max(List, {301, Y}, L, Sum, {MaxPower, P, Size}) -> find_max(List, {1, Y + 1}, L, Sum, {MaxPower, P, Size});
+find_max(List, {X, Y}, L, Sum, {MaxPower, P, Size}) when X + L > 300; Y + L > 300 ->
+	find_max(List, {X + 1, Y}, 0, Sum, {MaxPower, P, Size});
+find_max(List, {X, Y}, L, Sum, {MaxPower, P, Size}) ->
+	Cols = lists:nth(X + L, List),
+	Lines = lists:map(fun(LL) -> lists:nth(Y + L, LL) end, List),
+	io:format("STATE: ~p~n", [{Cols, Lines, {X, Y}, L}]),
+	NewSum = Sum + get_sum(Cols, Lines, L, 0, {X, Y}),
+	case NewSum > MaxPower of
+		true -> find_max(List, {X, Y}, L + 1, NewSum, {NewSum, {X, Y}, (L + 1) * (L + 1)});
+		false -> find_max(List, {X, Y}, L + 1, NewSum, {MaxPower, P, Size})
 	end.
+
+get_sum([H|_], [H|_], 0, Sum, _) -> Sum + H;
+get_sum([Hc|Tc], [Hl|Tl], N, Sum, {0, 0}) -> get_sum(Tc, Tl, N - 1, Sum + Hc + Hl, {0, 0});
+get_sum(Rows, [_|Tl], N, Sum, {0, Y}) -> 
+	io:format("0000: ~p~n", [{Tl, N}]),
+	get_sum(Rows, Tl, N, Sum, {0, Y - 1});
+get_sum([_|Tc], Lines, N, Sum, {X, Y}) ->
+	io:format("~p~n", [Tc]),
+	get_sum(Tc, Lines, N, Sum, {X - 1, Y}).
+
+%% part 1
 
 process1(GridSerialNumber) ->
-	process1(GridSerialNumber, {1, 1}, #{}, {nil, -45}, 2).
+	process1(GridSerialNumber, {1, 1}, #{}, {nil, -45}).
 
-process1(_, {X, Y}, Map, Res, L) when L + X > 300, L + Y > 300 -> {Map, Res};
-process1(GSN, {X, Y}, Map, Res, L) when L + X > 300 -> process1(GSN, {1, Y + 1}, Map, Res, L);
-process1(GSN, {X, Y}, Map, {P, Power}, L) ->
-	{NewPower, NewMap} = calculate_size({X, Y}, {X + L, Y + L}, 0, Map, GSN, L),
+process1(_, {299, 299}, _, Res) -> Res;
+process1(GSN, {299, Y}, Map, Res) -> process1(GSN, {1, Y + 1}, Map, Res);
+process1(GSN, {X, Y}, Map, {P, Power}) ->
+	{NewPower, NewMap} = calculate_size({X, Y}, {X + 2, Y + 2}, 0, Map, GSN),
 	case NewPower > Power of
-		true -> process1(GSN, {X + 1, Y}, NewMap, {{X, Y}, NewPower}, L);
-		false -> process1(GSN, {X + 1, Y}, NewMap, {P, Power}, L)
+		true -> process1(GSN, {X + 1, Y}, NewMap, {{X, Y}, NewPower});
+		false -> process1(GSN, {X + 1, Y}, NewMap, {P, Power})
 	end.
 
-calculate_size({_, _}, {M, N}, Power, Map, _, _) when M > 299; N > 299 -> {Power, Map};
-calculate_size({_, Y}, {_, N}, Power, Map, _, _) when Y > N -> {Power, Map};
-calculate_size({X, Y}, {M, N}, Power, Map, V, L) when X > M -> calculate_size({M - L, Y + 1}, {M, N}, Power, Map, V);
-calculate_size({X, Y}, {M, N}, Power, Map, V, L) ->
+calculate_size({_, Y}, {_, N}, Power, Map, _) when Y > N -> {Power, Map};
+calculate_size({X, Y}, {M, N}, Power, Map, V) when X > M -> calculate_size({M - 2, Y + 1}, {M, N}, Power, Map, V);
+calculate_size({X, Y}, {M, N}, Power, Map, V) ->
 	case maps:get({X, Y}, Map, nil) of
 		nil -> P = calculate_power({X, Y}, V),
-			   calculate_size({X + 1, Y}, {M, N}, Power + P, maps:put({X, Y}, P, Map), V, L);
-		S -> calculate_size({X + 1, Y}, {M, N}, Power + S, Map, V, L)
+			   calculate_size({X + 1, Y}, {M, N}, Power + P, maps:put({X, Y}, P, Map), V);
+		S -> calculate_size({X + 1, Y}, {M, N}, Power + S, Map, V)
 	end.
 
 calculate_power({X, Y}, GSN) ->
